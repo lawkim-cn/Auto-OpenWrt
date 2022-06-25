c5c41f081001853035406a508fc64e92126fb147
Manjusaka <me@manjusaka.me>
2022-06-26 00:04:07 +0800

    update code

--- diy-5.15-part2.sh
+++ diy-5.15-part2.sh
@@ -49,8 +49,7 @@ cd ~/work/Auto-OpenWrt/Auto-OpenWrt/openwrt
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 # Modify default banner
-cd ~/work/Auto-OpenWrt/Auto-OpenWrt/openwrt
 rm -rf package/base-files/files/etc/banner
-cp -f ../banner package/base-files/files/etc/banner
+cp -f ~/work/Auto-OpenWrt/Auto-OpenWrt/openwrt/banner package/base-files/files/etc/banner
 
 
